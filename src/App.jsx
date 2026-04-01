36c4b003b6ff2f57eaf6a571f057fc7ef0560a30
melio0504 <82772382+melio0504@users.noreply.github.com>
2026-04-01 14:57:45 +0800

    Import components to the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import { useRef, useState } from 'react'
+import jsPDF from 'jspdf'
 import CV from './components/CV'
+import Form from './components/Form'
+import './styles/CV.css'
 
 export default function App() {
   return (
